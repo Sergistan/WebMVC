6df778e01e05383b7b43aa332741b6f26ef1a0a1
Sergei Utochkin <dzaga73i98@gmail.com>
2023-04-18 19:27:02 +0300

    add DTO

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,40 +1,55 @@
 package ru.netology.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.netology.dto.PostDto;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService service;
+    private final ModelMapper modelMapper;
 
-    public PostController(PostService service) {
+    @Autowired
+    public PostController(PostService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public List<Post> all() {
-        return service.all();
+    public List<PostDto> all() {
+        return service.all().stream().map(this::convertToPersonDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Post getById(@PathVariable long id) {
-        return service.getById(id);
+    public PostDto getById(@PathVariable long id) {
+        return convertToPersonDto(service.getById(id));
     }
 
     @PostMapping
-    public Post save(@RequestBody Post post) {
-        return service.save(post);
+    public Post save(@RequestBody PostDto postDto) {
+        return service.save(convertToPerson(postDto));
     }
 
     @DeleteMapping("/{id}")
     public void removeById(@PathVariable long id) {
         service.removeById(id);
+    }
+
+    private Post convertToPerson(PostDto postDto) {
+        return modelMapper.map(postDto, Post.class);
+    }
+
+    private PostDto convertToPersonDto(Post post) {
+        return modelMapper.map(post, PostDto.class);
     }
 }
 
